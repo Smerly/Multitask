8e20c483b5da56381d6319dd5e59a3d47f735e02
RogueNPC <jordanbtansay@gmail.com>
2022-06-30 15:12:47 -0700

    Added event script placeholders for buttons

---
+++ client/src/scripts.js
@@ -0,0 +1,10 @@
+audio.addEventListener('click', toggleMute)
+video.addEventListener('click', toggleVideo)
+
+function toggleMute() {
+    alert('You clicked the mute button.')
+}
+
+function toggleVideo() {
+    alert('You clicked the video button.')
+}
