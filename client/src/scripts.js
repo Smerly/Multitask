0cc8d133a937d23f095d7d266a100630d5880210
RogueNPC <jordanbtansay@gmail.com>
2022-07-07 16:08:28 -0700

    added applescript node package

--- client/src/scripts.js
+++ client/src/scripts.js
@@ -2,13 +2,18 @@
 // (maybe through terminal/bash commands using "osascript zoomvideo.scpt")
 
 // Event Listners for our html buttons that will be able to execute commands
+
+const applescript = require('applescript');
+
 audio.addEventListener('click', toggleMute)
 video.addEventListener('click', toggleVideo)
 
 function toggleMute() {
+    applescript.execFile("./applescripts/zoomaudio.scpt")
     alert('You clicked the mute button.')
 }
 
 function toggleVideo() {
+    applescript.execFile("./applescripts/zoomvideo.scpt")
     alert('You clicked the video button.')
 }
