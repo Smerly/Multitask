70466c07e9cca0439863e657cdafbf46864d88f2
Ernest Cheung <ernestcheung02@gmail.com>
2022-07-10 17:04:44 -0700

    Super quick developer tool removal that I forgot about

--- client/src/index.js
+++ client/src/index.js
@@ -41,23 +41,23 @@ const createWindow = () => {
 	});
 	// Main window
 	const mainWindow = new BrowserWindow({
-		// width: 150,
-		// height: 500,
-		// minWidth: 150,
-		// maxWidth: 150,
-
-		width: 500,
+		width: 150,
 		height: 500,
-		maxHeight: 500,
-		minHeight: 500,
-		minWidth: 500,
-		maxWidth: 500,
+		minWidth: 150,
+		maxWidth: 150,
+
+		// width: 500,
+		// height: 500,
+		// maxHeight: 500,
+		// minHeight: 500,
+		// minWidth: 500,
+		// maxWidth: 500,
 		border: '1px black solid',
 		frame: false,
 		autoHideMenuBar: true,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
-			// devTools: false,
+			devTools: false,
 		},
 		transparent: true,
 		alwaysOnTop: true,
